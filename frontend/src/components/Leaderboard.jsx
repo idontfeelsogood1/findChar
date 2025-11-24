fad52ab2a425afaa03b39749a3695b8eea3dd3fa
Le Duong <leduongnewmemes123@gmail.com>
2025-11-24 17:53:02 +0700

    feat(frontend): Finish implementation

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,7 +1,48 @@
+import { useFetch } from "../api/hooks"
+
 export default function Leaderboard() {
+    const url = import.meta.env.VITE_SERVER_URL + `/leaderboard`
+    const { data, loading, error } = useFetch(url) 
+    let leaderboard = null
+
+    if (loading) {
+        return (
+            <h1>Loading leaderboard...</h1>
+        )
+    }
+    if (error) {
+        return (
+            <div>
+                {error.message}
+                {error.status}
+            </div>
+        )
+    }
+    if (data) {
+        leaderboard = data.leaderboard
+    }
+
     return (
-        <div>
-            Leaderboard
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Player Name</th>
+                    <th>Seconds</th>
+                    <th>Game Name</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {leaderboard.map((item) => {
+                    return (
+                        <tr>
+                            <td>{item.username}</td>
+                            <td>{item.seconds}</td>
+                            <td>{item.gamename}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
     )
 }
