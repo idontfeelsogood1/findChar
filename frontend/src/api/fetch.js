0bea46ed49eda0f9124e71ce0e99e7ceb86a305d
Le Duong <leduongnewmemes123@gmail.com>
2025-11-24 17:17:52 +0700

    feat(frontend): Implement postLeaderboard

--- frontend/src/api/fetch.js
+++ frontend/src/api/fetch.js
@@ -10,7 +10,7 @@ export async function fetchResult(gameId, characterId, clickX, clickY) {
             body: JSON.stringify({ 
                 clickX: clickX, 
                 clickY: clickY
-            })
+              })
         })
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -21,3 +21,27 @@ export async function fetchResult(gameId, characterId, clickX, clickY) {
         throw err
     }
 }
+
+export async function postLeaderboard(username, gamename, seconds) {
+    const url = import.meta.env.VITE_SERVER_URL + `/leaderboard`
+    try {
+        const response = await fetch(url, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({ 
+                username: username,
+                gamename: gamename,
+                seconds: seconds,
+            })
+        })
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    } catch(err) {
+        console.log("Error at fetchResult: ", err)
+        throw err
+    }
+}
+
