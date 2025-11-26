486b98d8fd958469424b73636ece917fa6d30a9d
Le Duong <leduongnewmemes123@gmail.com>
2025-11-26 13:35:51 +0700

    feat(frontend): Implement component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import style from "../components-css/Footer.module.css"
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+            <h3>idontfeelsogood © 2025</h3>
+        </footer>
+    )
+}
