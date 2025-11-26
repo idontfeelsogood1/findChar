7545406737220576d31ca2463c6dd57d16f58d80
Le Duong <leduongnewmemes123@gmail.com>
2025-11-26 14:36:15 +0700

    change(frontend): Change getting game image from backend to frontend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,32 +1,31 @@
 import style from "../components-css/Home.module.css"
 
 export default function Home() {
-    console.log(import.meta.env.VITE_SERVER_URL)
     return (
         <main className={style.main}>
             <h1 className={style.pickGame}>Pick a game</h1>
             <div className={style.container}>
                 <div className={style.imgContainer}>
                     <a href="/game/1">
-                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/cityport.png"} alt="cityport.png" />
+                        <img className={style.customImg} src="/images/cityport.png" alt="cityport.png" />
                     </a>
                 </div>
 
                 <div className={style.imgContainer}>
-                    <a href="/game/2">
-                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/rainforest.png"} alt="rainforest.png" />
+                    <a href="/gamåe/2">
+                        <img className={style.customImg} src="/images/rainforest.png" alt="rainforest.png" />
                     </a>
                 </div>
 
                 <div className={style.imgContainer}>
                     <a href="/game/3">
-                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/floating_island.png"} alt="floating_island.png" />
+                        <img className={style.customImg} src="/images/floating_island.png" alt="floating_island.png" />
                     </a>
                 </div>
 
                 <div className={style.imgContainer}> 
                     <a href="/game/4">
-                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/medieval_floating_village.png"} alt="medieval_floating_village.png" />
+                        <img className={style.customImg} src="/images/medieval_floating_village.png" alt="medieval_floating_village.png" />
                     </a>
                 </div>
             </div>
