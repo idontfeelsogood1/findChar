745b9275330793810ffe9abb41db484edbfd6bc9
Le Duong <leduongnewmemes123@gmail.com>
2025-11-26 13:33:32 +0700

    feat(frontend): Add styles

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,30 +3,33 @@ import style from "../components-css/Home.module.css"
 export default function Home() {
     console.log(import.meta.env.VITE_SERVER_URL)
     return (
-        <div className={style.container}>
-            <div>
-                <a href="/game/1">
-                    <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/cityport.png"} alt="cityport.png" />
-                </a>
-            </div>
+        <main className={style.main}>
+            <h1 className={style.pickGame}>Pick a game</h1>
+            <div className={style.container}>
+                <div className={style.imgContainer}>
+                    <a href="/game/1">
+                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/cityport.png"} alt="cityport.png" />
+                    </a>
+                </div>
 
-            <div>
-                <a href="/game/2">
-                    <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/rainforest.png"} alt="rainforest.png" />
-                </a>
-            </div>
+                <div className={style.imgContainer}>
+                    <a href="/game/2">
+                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/rainforest.png"} alt="rainforest.png" />
+                    </a>
+                </div>
 
-            <div>
-                <a href="/game/3">
-                    <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/floating_island.png"} alt="floating_island.png" />
-                </a>
-            </div>
+                <div className={style.imgContainer}>
+                    <a href="/game/3">
+                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/floating_island.png"} alt="floating_island.png" />
+                    </a>
+                </div>
 
-            <div>
-                <a href="/game/4">
-                    <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/medieval_floating_village.png"} alt="medieval_floating_village.png" />
-                </a>
+                <div className={style.imgContainer}> 
+                    <a href="/game/4">
+                        <img className={style.customImg} src={import.meta.env.VITE_SERVER_URL + "/images/medieval_floating_village.png"} alt="medieval_floating_village.png" />
+                    </a>
+                </div>
             </div>
-        </div>
+        </main>
     )
 }
