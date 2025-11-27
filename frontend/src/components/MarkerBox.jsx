7cfb07788372da43a3844f427d20448e09aa4775
Le Duong <leduongnewmemes123@gmail.com>
2025-11-27 13:40:01 +0700

    feat(frontend): Finish implementation

---
+++ frontend/src/components/MarkerBox.jsx
@@ -0,0 +1,24 @@
+import style from '../components-css/MarkerBox.module.css'
+
+export default function MarkerBox({ boxInfos }) {
+    return (
+        boxInfos.map((info) => {
+            return (
+                <div 
+                    className={style.container}
+                    // Necessary for functionality
+                    style={{
+                        position: "absolute",
+                        zIndex: 9998,
+                        top: info.y,
+                        left: info.x,
+                        width: info.width,
+                        height: info.height,
+                        backgroundColor: "black" 
+                    }}
+                >
+                </div>
+            )
+        })
+    )
+}
