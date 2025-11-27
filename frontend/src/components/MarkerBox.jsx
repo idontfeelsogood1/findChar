414053171b363c55de2f5b32bd01c4c14eade673
Le Duong <leduongnewmemes123@gmail.com>
2025-11-27 13:58:07 +0700

    feat(frontend): Finish implementation

--- frontend/src/components/MarkerBox.jsx
+++ frontend/src/components/MarkerBox.jsx
@@ -16,6 +16,11 @@ export default function MarkerBox({ boxInfos }) {
                         height: info.height,
                     }}
                 >
+                    <svg className={style.customSvg} width="100%" height="100%" style={{ display: 'block' }}>
+                        <rect width="100%" height="100%" />
+                        <line x1="0" y1="0" x2="100%" y2="100%" />
+                        <line x1="100%" y1="0" x2="0" y2="100%" />
+                    </svg>
                 </div>
             )
         })
