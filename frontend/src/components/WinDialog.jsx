31517cafd2990632861a16e826744e24717037a0
Le Duong <leduongnewmemes123@gmail.com>
2025-11-27 15:14:31 +0700

    feat(frontend): Add styling

--- frontend/src/components/WinDialog.jsx
+++ frontend/src/components/WinDialog.jsx
@@ -1,5 +1,6 @@
 import { postLeaderboard } from "../api/fetch"
 import { useNavigate } from "react-router"
+import style from "../components-css/WinDialog.module.css"
 
 export default function WinDialog({ dialogRef, gamename, seconds }) {
     const navigate = useNavigate()
@@ -14,15 +15,15 @@ export default function WinDialog({ dialogRef, gamename, seconds }) {
     }
 
     return (
-        <dialog ref={dialogRef}>
-            <div>
+        <dialog className={style.customDiag} ref={dialogRef}>
+            <header className={style.customHeader}>
                 <h2>You found all the characters!</h2>
                 <p>Time: {seconds} seconds</p>
-            </div>
-            <form onSubmit={handleSubmit}>
+            </header>
+            <form className={style.customForm} onSubmit={handleSubmit}>
                 <label htmlFor="username">Enter your name for leaderboard:</label>
                 <p>
-                    <input type="text" id="username" name="username"/>
+                    <input type="text" id="username" name="username" placeholder="Enter your name here..."/>
                 </p>
                 <button type="submit">Submit</button>
             </form>
