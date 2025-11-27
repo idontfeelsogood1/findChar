3165b2b49e991436e8fea007e853eaf1d9c87eae
Le Duong <leduongnewmemes123@gmail.com>
2025-11-27 13:38:58 +0700

    feat(frontend): Implement functionality to render a marker box around hit characters

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -3,29 +3,62 @@ import { useFetch } from '../api/hooks';
 import { useState, useEffect } from 'react';
 import Menu from './Menu';
 import Dialog from './Dialog';
+import MarkerBox from './MarkerBox';
 import style from '../components-css/Game.module.css'
 
 export default function Game() {
     const { gameId } = useParams()
-    const [imgNormalizedPosition, setImgNormalizedPosition] = useState({ x: 0, y: 0})
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0})
-    const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0})
+    const [imgNormalizedPosition, setImgNormalizedPosition] = useState({ x: 0, y: 0 })
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
+    const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 })
+    const [imgSize, setImgSize] = useState({ 
+        baseWidth: 0, 
+        baseHeight: 0, 
+        newWidth: 0,
+        newHeight: 0
+    })
     const [showMenu, setShowMenu] = useState(false)
     const [showDialog, setShowDialog] = useState(false)
+    const [boxInfos, setBoxInfos] = useState([])
     const [result, setResult] = useState(null)
     const url = import.meta.env.VITE_SERVER_URL + `/game/${gameId}`
     const { data, loading, error } = useFetch(url)
+    let game = null,
+        characters = null
 
-    // Show dialog on result
     useEffect(() => {
+        // Show dialog on result
         setShowDialog(true)
         setTimeout(() => {
             setShowDialog(false)
         }, "2000")
-    }, [result])
 
-    let game = null,
-        characters = null
+        if (result) {
+            if (result.isCorrectClick) {
+                // Find the character of result
+                const { x, y, width, height } = characters.find((char) => char.id === result.characterId)
+  
+                // NORMALIZE CHARACTERS BASE x, y, width, height INTO NEW newX, newY, newWidth, newHeight
+                const scaleX = imgSize.newWidth / imgSize.baseWidth, 
+                      scaleY = imgSize.newHeight / imgSize.baseHeight
+        
+                const newX = x * scaleX,
+                    newY = y * scaleY,
+                    newWidth= width * scaleX,
+                    newHeight = height * scaleY 
+
+                setBoxInfos([
+                    ...boxInfos,
+                    {
+                        x: newX,
+                        y: newY,
+                        width: newWidth,
+                        height: newHeight,
+                    }
+                ])
+            }
+        }
+    }, [result])
 
     if (loading) {
         return (
@@ -62,6 +95,12 @@ export default function Game() {
             setShowMenu(true)
             setMenuPosition({ x: event.pageX, y: event.pageY})
             setCursorPosition({ x: event.pageX, y: event.pageY})
+            setImgSize({
+                baseWidth: game.baseWidth, 
+                baseHeight: game.baseHeight, 
+                newWidth: event.currentTarget.offsetWidth,
+                newHeight: event.currentTarget.offsetHeight
+            })
             const { normalizedX, normalizedY } = normalizeImgCoords(
                                                     event.nativeEvent.offsetX, 
                                                     event.nativeEvent.offsetY,
@@ -94,6 +133,7 @@ export default function Game() {
             
             <main className={style.imgContainer}>
                 <img className={style.customImg} src={game.imgUrl} alt={game.imgPath} onClick={handleClick}/>
+                <MarkerBox boxInfos={boxInfos} />
             </main>
 
             <Menu 
