31b9aa09b717ccc871a5b52d5e9a16c35ac1396a
Le Duong <leduongnewmemes123@gmail.com>
2025-11-28 14:19:52 +0700

    fix(backend): Add secure: depending on NODE_ENV

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(
     cookie: {
      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
      sameSite: 'none',
-     secure: process.env.NODE_ENV === "development",
+     secure: process.env.NODE_ENV != "development",
     },
     secret: process.env.SESSION_SECRET,
     resave: false,
